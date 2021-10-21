16b9bc6ed9f167fafdde7a74b2d4cb84ea03dd72
Maddie <maddie.comtois@gmail.com>
2021-10-21 14:37:49 +0100

    create UI
    - add rdfstore to backend
    - send queries from front to back end
    - display queries on front end

--- ontology-project/api/server.js
+++ ontology-project/api/server.js
@@ -2,19 +2,54 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 var rdfstore = require('rdfstore');
+var fs = require('fs');
+const cors = require('cors');
 
-
+// set up Express
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const port = process.env.PORT || 4000;
 
-const server = app.listen(port, function(){
-    console.log('Listening on port ' + port);
+const server = app.listen(port, function() {
+  console.log('Listening on port ' + port);
 });
 
-new rdfstore.Store(function(err, store) {
-  // the new store is ready
+// list of queries to test
+var queries = {
+  1: "SELECT * WHERE { ?s ?p ?o } LIMIT 1",
+  2: "SELECT * WHERE { ?s ?p ?o } LIMIT 2",
+  3: "SELECT * WHERE { ?s ?p ?o } LIMIT 3",
+  4: "SELECT * WHERE { ?s ?p ?o } LIMIT 4",
+  5: "SELECT * WHERE { ?s ?p ?o } LIMIT 5",
+  6: "SELECT * WHERE { ?s ?p ?o } LIMIT 6",
+  7: "SELECT * WHERE { ?s ?p ?o } LIMIT 7",
+  8: "SELECT * WHERE { ?s ?p ?o } LIMIT 8",
+  9: "SELECT * WHERE { ?s ?p ?o } LIMIT 9",
+  10: "SELECT * WHERE { ?s ?p ?o } LIMIT 10",
+}
+
+// convert rdf data file into a string
+var rdf = fs.readFileSync('CovidCountyStatistics.ttl').toString();
+
+// create an instance of an rdf store to search
+var store = rdfstore.create(function(err, store) {});
+
+// return the sparql result from one of the listed queries (query identifed by number from client end)
+app.get('/getQueryResponse/:queryId', (req, res) => {
+  // load the data string into the store
+  store.load('text/turtle', rdf, function(err) {
+    if (!err) {
+      // execute the query and return the response
+      store.execute(queries[req.params.queryId], function(success, results) {
+        res.json(results);
+      });
+    }
+    else {
+      res.json(err);
+    }
+  });
 });
 
 module.exports = app;
--- ontology-project/src/app/app.component.ts
+++ ontology-project/src/app/app.component.ts
@@ -1,21 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { AppServiceService } from './app-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  constructor(
-  ) {}
+  // array to store query responses from server
+  queryResponses: Query[] = [];
   
+  // query string options displayed on the buttons
+  query1 = "SELECT * WHERE { ?s ?p ?o } LIMIT 1";
+  query2 = "SELECT * WHERE { ?s ?p ?o } LIMIT 2";
+  query3 = "SELECT * WHERE { ?s ?p ?o } LIMIT 3";
+  query4 = "SELECT * WHERE { ?s ?p ?o } LIMIT 4";
+  query5 = "SELECT * WHERE { ?s ?p ?o } LIMIT 5";
+  query6 = "SELECT * WHERE { ?s ?p ?o } LIMIT 6";
+  query7 = "SELECT * WHERE { ?s ?p ?o } LIMIT 7";
+  query8 = "SELECT * WHERE { ?s ?p ?o } LIMIT 8";
+  query9 = "SELECT * WHERE { ?s ?p ?o } LIMIT 9";
+  query10 = "SELECT * WHERE { ?s ?p ?o } LIMIT 10";
   
+  constructor( private service : AppServiceService) {}
+  
+  // function that gets called on loading component (can delete if don't use)
   ngOnInit() {
-  
+    console.log("component loaded");
   }
   
-  
+  // send whichever query to server based on which button was pressed
+  sendQuery(queryId) {
+    this.queryResponses = [];
+    // call function in service file to send/get data over http
+    this.service.getQueryResponse(queryId).subscribe((res: Query[]) => {
+      for(let i = 0; i < res.length; i++) {
+        this.queryResponses.push(res[i]);
+      }
+      console.log(this.queryResponses);
+    });
+  }  
+}
+
+// class to store the different attributes of a query response
+export class Query {
+    s: Object;
+    o: Object;
+    p: Object;
 }
